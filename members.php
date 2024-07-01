0d8d5387bc2192870064c351e511aec6f14ba24a
Adi <adiaimanputra.work@gmail.com>
2024-07-02 04:44:07 +0800

    members page dah ubah

--- members.php
+++ members.php
@@ -1,13 +1,4 @@
 <!--------------------------------------------CSS-------------------------------------------->
-<style>
-    .postImage {
-        max-width: 4em;
-        max-height: 4em;
-        border-radius: 1em;
-        object-fit: cover;
-    }
-</style>
-
 <?php
 $pageCSS = ob_get_clean();
 
@@ -15,49 +6,50 @@ ob_start();
 ?>
 <!------------------------------------------Content------------------------------------------>
 <head>
-    <h2>CDCS110 (DIPLOMA IN COMPUTER SCIENCE)</h2>
-    <h2>CSC264 GROUP PROJECT JCDCS1104B</h2>
+    <h2 style="font-family:Courier New">CDCS110 (DIPLOMA IN COMPUTER SCIENCE)</h2>
+    <h2 style="font-family:Courier New">CSC264 GROUP PROJECT JCDCS1104B</h2>
 
 </head>
+<body style="background-color: #B6D0E2;">
 <table class="table">
   <thead>
     <tr>
-      <th scope="col">No</th>
-      <th scope="col">Name</th>
-      <th scope="col">Picture</th>
-      <th scope="col">Student Number</th>
-      <th scope="col">Email</th>
+      <th style="font-family:Courier New" scope="col">No</th>
+      <th style="font-family:Courier New" scope="col">Name</th>
+      <th style="font-family:Courier New" scope="col">Picture</th>
+      <th style="font-family:Courier New" scope="col">Student Number</th>
+      <th style="font-family:Courier New" scope="col">Email</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>ADI AIMAN PUTRA</td>
-      <td><img class="postImage" src="assets/membersImage/adi aiman.png" width="100" height="100"></td>
-      <td>2022897414</td>
-      <td>2022897414@student.uitm.edu.my</td>
+      <th style="font-family:Courier New" scope="row">1</th>
+      <td style="font-family:Courier New">ADI AIMAN PUTRA</td>
+      <td><img class="image" src="assets/membersImage/adi aiman.png" width="100" height="100"></td>
+      <td style="font-family:Courier New">2022897414</td>
+      <td style="font-family:Courier New">2022897414@student.uitm.edu.my</td>
 
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>NURUL UMAIRAH SUHANA BINTI KHALILUDDIN</td>
-      <td><img class="postImage" src="assets/membersImage/umairah suhana.png" width="100" height="100"></td>
-      <td>2022825646</td>
-      <td>2022825646@student.uitm.edu.my</td>
+      <th style="font-family:Courier New" scope="row">2</th>
+      <td style="font-family:Courier New">NURUL UMAIRAH SUHANA BINTI KHALILUDDIN</td>
+      <td><img class="image" src="assets/membersImage/umairah suhana.png" width="100" height="100"></td>
+      <td style="font-family:Courier New">2022825646</td>
+      <td style="font-family:Courier New">2022825646@student.uitm.edu.my</td>
     </tr>
     <tr>
-      <th scope="row">3</th>
-      <td>FARAH IZZATI BINTI ZAINALABIDDIN</td>
-      <td><img class="postImage" src="assets/membersImage/farah izzati.png" width="100" height="100"></td>
-      <td>2022600256</td>
-      <td>2022600256@student.uitm.edu.my</td>
+      <th style="font-family:Courier New" scope="row">3</th>
+      <td style="font-family:Courier New">FARAH IZZATI BINTI ZAINALABIDDIN</td>
+      <td><img class="image" src="assets/membersImage/farah izzati.png" width="100" height="100"></td>
+      <td style="font-family:Courier New">2022600256</td>
+      <td style="font-family:Courier New">2022600256@student.uitm.edu.my</td>
     </tr>
     <tr>
-      <th scope="row">4</th>
-      <td>MOHAMMED ZUHAYR BIN MOHD ZAKI</td>
-      <td><img class="postImage" src="assets/membersImage/zuha.png" width="100" height="100"></td>
-      <td>2022808754</td>
-      <td>2022808754@student.uitm.edu.my</td>
+      <th style="font-family:Courier New" scope="row">4</th>
+      <td style="font-family:Courier New">MOHAMMED ZUHAYR BIN MOHD ZAKI</td>
+      <td><img class="image" src="assets/membersImage/zuha.png" width="100" height="100"></td>
+      <td style="font-family:Courier New">2022808754</td>
+      <td style="font-family:Courier New">2022808754@student.uitm.edu.my</td>
     </tr>
   </tbody>
 </table>
